f602c6623874219c417cb81af362e85a289a472b
Tomas Jramoy <tomijramoy@gmail.com>
2022-10-28 09:35:09 -0300

    28/10

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import vistas.FrmMenuPrincipal;
-
-public class Main {
-    public static void main(String[] args) {
-        FrmMenuPrincipal win = new FrmMenuPrincipal();
-        win.setVisible(true);
-    }
-}
