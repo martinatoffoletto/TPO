d2820c0b3aad83ce1ab537e24a368720321720e4
Tomas Jramoy <tomijramoy@gmail.com>
2022-10-28 08:04:40 -0300

    21:41 27/10

---
+++ src/vistas/FrmControllerParametros.java
@@ -0,0 +1,16 @@
+package vistas;
+
+import javax.swing.*;
+
+public class FrmControllerParametros extends JInternalFrame {
+    private JPanel pnlPrincipal;
+    private JButton altaUsuarioButton;
+
+    public FrmControllerParametros() {
+        super("Controlador de Parámtros");
+        setBorder(null);
+        ((javax.swing.plaf.basic.BasicInternalFrameUI)this.getUI()).setNorthPane(null);
+        setContentPane(pnlPrincipal);
+
+    }
+}
