6851fdc6792b2047bf798dc585b1b82e3288a79e
Tomas Jramoy <tomijramoy@gmail.com>
2022-10-30 20:05:17 -0300

    30/10 20:05

---
+++ src/vista/FrmModificarUsuario.java
@@ -0,0 +1,15 @@
+package vista;
+
+import javax.swing.*;
+
+public class FrmModificarUsuario {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JPasswordField passwordField1;
+    private JComboBox comboBox1;
+    private JButton guardarButton;
+}
---
+++ src/vista/FrmUsuario.java
@@ -0,0 +1,11 @@
+package vista;
+
+import javax.swing.*;
+
+public class FrmUsuario {
+    private JList list1;
+    private JPanel panel1;
+    private JButton nuevoUsuarioButton;
+    private JButton modificarUsuarioButton;
+    private JButton eliminarUsuarioButton;
+}
